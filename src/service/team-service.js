852a0638eeab0e614c484008386873f7380ee4d8
henricker <henriquevieira@alu.ufc.br>
2022-10-17 10:08:12 -0300

    feat: add function get team

--- src/service/team-service.js
+++ src/service/team-service.js
@@ -6,8 +6,30 @@ class TeamService {
         this.teamRepository = teamRepository
     }
 
-    getTeam() {
-        
+    getRandomElementFromArray(list) {
+        return list[Math.floor(Math.random() * list.length)]
+    }
+
+    getMultipleElementsFromArray(list, quantity) {
+       return Array(quantity).fill(quantity).map(() => this.getRandomElementFromArray(list))
+    }
+
+    async getTeam() {
+        const pokemonList = await this.teamRepository.listPokemons()
+        const randomTeam = this.getMultipleElementsFromArray(pokemonList, 3)
+
+        return await Promise.all(
+            randomTeam.map(async pokemon => {
+                const { url } = pokemon
+                const pokemonInfo = await this.teamRepository.getPokemon(url)
+            
+                const moves = pokemonInfo.moves.splice(0, 3)
+                return {
+                   name: pokemonInfo.name,
+                   moves
+                }
+            })
+        )
     }
 }
 
