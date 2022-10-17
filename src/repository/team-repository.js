b052ffd85e8e3a5415afcf9dcbad2083c1aa3e98
henricker <henriquevieira@alu.ufc.br>
2022-10-17 09:02:14 -0300

    feat: should make request to list and get specified pokemon

--- src/repository/team-repository.js
+++ src/repository/team-repository.js
@@ -3,7 +3,7 @@ const https = require('https');
 const API_URL = 'https://pokeapi.co/api/v2'
 
 class TeamRepository {
-    async #createRequest(url) {
+    async createRequest(url) {
        return new Promise((resolve, reject) => {
             https.get(url, (response) => {
                 const chunks = [];
@@ -19,6 +19,23 @@ class TeamRepository {
                 })
             })
        })
+    }
+
+    async listPokemons() {
+        const url = `${API_URL}/pokemon`
+        const response = await this.createRequest(url)
+        return JSON.parse(response).results
+    }
+
+    async getPokemon(url) {
+        const response = await this.createRequest(url)
+        const result = JSON.parse(response).results
+
+        const pokemon = {
+            name: result.name,
+            moves: result.moves.map(move => move.move.name),
+        }
+        return pokemon
     }
 }
 
